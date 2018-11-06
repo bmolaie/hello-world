e85cf52baed673cd5520d88f4f060d3ad4c8ac6b
bmolaie <bmolaie38@gmail.com>
2018-11-06 13:19:35 -0500

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+function helloConsole(){
+  console.log("Hello, World!");
+}
+function helloPopup(){
+  alert("Hello, World!");
+}
+function helloBrowser(){
+  var p=document.getElementById("helloBrowser");
+  p.innerHTML="Hello, World!";
+}
+function helloStranger(){
+var name=prompt("Type Name Here");
+alert('Hello, ' + name + '!')
+}
